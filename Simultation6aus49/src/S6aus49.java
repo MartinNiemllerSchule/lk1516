95eca55a0c431930a7381efc35a966077f7c16a9
Frank Baethge <baethge@kabelmail.de>
2015-07-27 10:33:28 +0200

    1000 Durchläufe, aber immernoch ohne grafisches Frontend Bei 100.000 Durchläufen macht sich die Laufzeit bemerkbar. Der Prozessor könnte besser ausgelastet werden.

--- Simultation6aus49/src/S6aus49.java
+++ Simultation6aus49/src/S6aus49.java
@@ -2,16 +2,23 @@ import java.util.Arrays;
 
 public class S6aus49 {
 	public static void main(String[] args) {
+		int maxDurchlauf = 1000;
+		int[] anzahlTreffer = new int[7];
+		int anzahlZiehungen = 0;
 		int[] tip = new Ziehung().getZiehung();  // Tip des Benutzers wird mit Zufallswerten belegt
 		System.out.println("Tip: " + Arrays.toString(tip));
-		int treffer = 0;
-		int anzahlZiehungen = 0;
-		while (treffer < 4) {
-			treffer = new Ziehung().getTreffer(tip);
-			anzahlZiehungen++;
+		for (int durchlauf=0; durchlauf < maxDurchlauf; durchlauf++ ) {
+			int treffer = 0;
+			while (treffer < 4) {
+				treffer = new Ziehung().getTreffer(tip);
+				anzahlZiehungen++;
+			}
+			anzahlTreffer[treffer]++;
 			System.out.print(treffer + " ");
-			if (anzahlZiehungen % 60 == 0) System.out.println();
+			if ((anzahlTreffer[4]+anzahlTreffer[5]+anzahlTreffer[6]) % 60 == 0) System.out.println();
 		}
-		System.out.println("\nnach " + anzahlZiehungen + " Ziehungen wurde ein " + treffer + "-er erziehlt.");
+		System.out.println("\nnach " + maxDurchlauf + " Durchläufen wurden\n " + anzahlTreffer[4] + " 4-er\n " +
+				anzahlTreffer[5] + " 5-er\n " +  anzahlTreffer[6] + " 6-er erzielt.");
+		System.out.println("Dazu waren durchschnittlich " + (anzahlZiehungen / maxDurchlauf) + " Ziehungen nötig.");
 	}
 }
