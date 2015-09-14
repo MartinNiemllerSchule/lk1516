f5ac0db9c5801377edbf839cd2cab34a00d3d16a
Frank Baethge <schule@baethge.info>
2015-09-14 21:32:14 +0200

    Kommentare ergänzt

--- keine7/src/keine7/Keine7.java
+++ keine7/src/keine7/Keine7.java
@@ -1,10 +1,10 @@
-/* Übung
-Es sollen von 100 Zufallszahlen aus dem Bereich von 1 bis 1000 die nicht durch 7 teilbaren und keine Ziffer 7 enthaltenden Zahlen ausgegeben werden
+/** Übung
+ * Es sollen von 100 Zufallszahlen aus dem Bereich von 1 bis 1000 die nicht durch 7 teilbaren und
+ * keine Ziffer 7 enthaltenden Zahlen ausgegeben werden
  */
 package keine7;
 
 /**
- *
  * @author frank.baethge
  */
 public class Keine7 {
@@ -13,7 +13,7 @@ public class Keine7 {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // 100 Zufalls-Zahlen abspeichern
+    // 100 Zufalls-Zahlen erzeugen, testen und gegebenenfalls ausgeben
     Integer zahl, anzahl = 0;
     for (Integer z = 1; z <= 100; z++) {
       zahl = (int)(Math.random()*1000);
@@ -23,8 +23,15 @@ public class Keine7 {
         anzahl++;
       }
     }
+    // zur Kontrolle ausgeben, wie viele Zahlen nicht ausgegeben wurden
      System.out.println("\nAnzahl: "+anzahl);
   }
+
+  /**
+   * ergibt wahr, falls keine Ziffer 7 enthalten ist und die Zahl nicht durch 7 teilbar ist
+   * @param zahl
+   * @return
+   */
   private static boolean istKeine7(Integer zahl) {
     boolean nichtTeilbar;
     boolean hatZiffer;
